8a7750c1fecc85dc67cb4fdd74cb3b42a35e6bc2
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2024-02-24 05:12:13 +0500

    Update csrm.py

--- csrm.py
+++ csrm.py
@@ -16,15 +16,15 @@ def dataentry():
             for keys, values in dict1.items():
                 st.write(f"{keys} : {values}")
         with col2:
-           # k = Image.open("yellowlamborghini.jpeg")
-          #  st.image(k, caption='Yellow Lamborghini', width = 210)
+            #k = Image.open("yellowlamborghini.jpeg")
+            #st.image(k, caption='Yellow Lamborghini', width = 210)
         with col3:
             dict1 = {'Model': 2010, 'Color': 'Black', 'Price' : 3520000}
             for keys, values in dict1.items():
                 st.write(f"{keys} : {values}")
         with col4:
-         #   k = Image.open("blacklamborghini.jpeg")
-         #   st.image(k, caption='Black Lamborghini',width = 210)        
+           # k = Image.open("blacklamborghini.jpeg")
+            #st.image(k, caption='Black Lamborghini',width = 210)        
 
         sel2 = st.checkbox('1990')
         sel3 = st.checkbox('2010')
@@ -128,15 +128,15 @@ def dataentry():
             for keys, values in dict1.items():
                 st.write(f"{keys} : {values}")
         with col2:
-          #  k = Image.open("greenmercedes.jpeg")
-           # st.image(k, caption='Green Mercedes')
+           # k = Image.open("greenmercedes.jpeg")
+            #st.image(k, caption='Green Mercedes')
         with col3:
             dict1 = {'Model': 2010, 'Color': 'Blue', 'Price' : 720000}
             for keys, values in dict1.items():
                 st.write(f"{keys} : {values}")
         with col4:
           #  k = Image.open("bluemercedes.jpeg")
-          #  st.image(k, caption='Blue Mercedes')        
+           # st.image(k, caption='Blue Mercedes')        
 
         sel2 = st.checkbox('1990')
         sel3 = st.checkbox('2010')
@@ -237,14 +237,14 @@ def dataentry():
                 st.write(f"{keys} : {values}")
         with col2:
            # k = Image.open("brownaudi.jpeg")
-          #  st.image(k, caption='Brown Audi')
+            #st.image(k, caption='Brown Audi')
         with col3:
             dict1 = {'Model': 2010, 'Color': 'Yellow', 'Price' : 150000}
             for keys, values in dict1.items():
                 st.write(f"{keys} : {values}")
         with col4:
-         #   k = Image.open("yellowaudi.jpeg")
-          #  st.image(k, caption='Yellow Audi')        
+          #  k = Image.open("yellowaudi.jpeg")
+           # st.image(k, caption='Yellow Audi')        
 
         sel2 = st.checkbox('1990')
         sel3 = st.checkbox('2010')
@@ -344,7 +344,7 @@ def dataentry():
             for keys, values in dict1.items():
                 st.write(f"{keys} : {values}")
         with col2:
-          #  k = Image.open("ferrariimage.jpeg")
+         #   k = Image.open("ferrariimage.jpeg")
           #  st.image(k, caption='Red Ferrari')
         with col3:
             dict1 = {'Model': 2010, 'Color': 'Black', 'Price' : 10000000}
@@ -352,7 +352,7 @@ def dataentry():
                 st.write(f"{keys} : {values}")
         with col4:
          #   k = Image.open("blackferrari.jpeg")
-         #   st.image(k, caption='Black Ferrari')        
+          #  st.image(k, caption='Black Ferrari')        
 
         sel2 = st.checkbox('1990')
         sel3 = st.checkbox('2010')
@@ -494,22 +494,6 @@ if 'page' not in st.session_state:
     st.session_state.page = 1
 
 st.header("Welcome to Our Car Selling Web Page")
-col1, col2, col3, col4 = st.columns(4)
-with col1:
-   # i = Image.open("C:/Users/MUHAMMAD WALEED/Desktop/streamlit tut/streamlitenv/Scripts/ferrariimage.jpeg")
-    #st.image(i, caption='Ferrari', width=160)
-
-with col2:
-  #  g = Image.open("lamborghiniimage.jpeg")
-   # st.image(g, caption='Lamborghini', width=170)
-
-with col3:
-  #  t = Image.open("audiimage.jpeg")
-  #  st.image(t, caption='Audi', width=160)
-
-with col4:
-  #  y = Image.open("mercedesimage.jpeg")
-  #  st.image(y, caption='Mercedes', width=170)    
     
 
 # Display image
